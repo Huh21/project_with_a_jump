f5cf5357ff824d84b103161a2ec8a7f3d3275420
DESKTOP-48D3MN0\erica <iyu20622@gmail.com>
2021-08-10 23:54:07 +0900

    수정하고 있는 부분 제외하고 기존 코드 가져옴

---
+++ app/src/main/java/org/ict/project_with_a_jump/FragmentGraph2.java
@@ -0,0 +1,24 @@
+package org.ict.project_with_a_jump;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class FragmentGraph2 extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_fragment_graph2, container, false);
+    }
+}
