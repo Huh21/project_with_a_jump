0338e9da2a75ad04e6c63ebdbe8e88224eeac4fe
hyeso <khwater1017@naver.com>
2021-09-11 07:36:07 +0900

    데이터베이스 저장

--- app/src/main/java/org/ict/project_with_a_jump/MainActivity.java
+++ app/src/main/java/org/ict/project_with_a_jump/MainActivity.java
@@ -5,15 +5,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainActivity<editText, editText2> extends AppCompatActivity {
-    EditText editText;
-    EditText editText2;
+
+    // 파이어베이스 데이터베이스 연동
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = database.getReference();
+
+    Button check;
+    EditText user_id,user_ad,user_sepw;
+
+
     public static WebView web;
     public static WebSettings webSettings;
+    private String Validation = "^.(?=.*[0-9])(?=.*[0-9ㄱ-ㅎ가-힣]).*$";
 
 
 
@@ -21,24 +37,50 @@ public class MainActivity<editText, editText2> extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        user_id = findViewById(R.id.user_id);
+        user_ad = findViewById(R.id.user_ad);
+        user_sepw = findViewById(R.id.user_sepw);
+
+
+        user_sepw.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    Pattern p = Pattern.compile("^[0-9ㄱ-ㅎ가-힣]+$");
+                    Matcher m = p.matcher((user_sepw).getText().toString());
+                    if ( !m.matches()){
+                        Toast.makeText(MainActivity.this, "안심번호 형식에 맞춰서 입력해주세요", Toast.LENGTH_SHORT).show();
+
+                    }
+
+
+                }
+            }
+        });
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //에딧 텍스트 값을 문자열로 바꾸어 함수에 넣어줍니다.
+                addUser(user_id.getText().toString(), user_sepw.getText().toString(), user_ad.getText().toString());
+            }
+        });
+
     }
 
-    private void singUp(){
-        String id= ((EditText)findViewById(R.id.user_id)).getText().toString();
-
+    private void addUser(String name, String num, String address) {
+        User User = new User(name, num, address);
     }
+
+
 
     public void onClick_login(View v) {
-        EditText user_id = (EditText) findViewById(R.id.user_id);
-        EditText user_ad = (EditText) findViewById(R.id.user_ad);
-        EditText user_password = (EditText) findViewById(R.id.user_password);
-        EditText user_passWordch= (EditText) findViewById(R.id.user_passwordch);
         String id= user_id.getText().toString();
-        String pw= user_password.getText().toString();
+        String sepw= user_sepw.getText().toString();
         String ad= user_ad.getText().toString();
         Intent intent_01= new Intent(getApplicationContext(), subActivity.class);
         intent_01.putExtra("입력한 이름",id);
-        intent_01.putExtra("입력한 패스워드",pw);
+        intent_01.putExtra("입력한 안심번호",sepw);
         intent_01.putExtra("입력한 주소",ad);
         startActivity(intent_01);
     }
---
+++ app/src/main/java/org/ict/project_with_a_jump/User.java
@@ -0,0 +1,39 @@
+package org.ict.project_with_a_jump;
+
+public class User {
+    String name; // 사용자의 이름
+    String num; // 사용자의 개인안심번호
+    String address; // 사용자의 간단한 거주지 주소
+
+    public User(){}
+
+    public User(String name, String num, String address) { // 사용자 정보 저장
+        this.name = name;
+        this.num = num;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum (String num) {
+        this.num = num;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress (String address) {
+        this.address = address;
+    }
+}
